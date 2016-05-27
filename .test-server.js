6a81241880db45045fbffaa52ff2b9e5984184e7
Dmitry Andriyanov <dima117a@yandex-team.ru>
2016-05-27 19:17:40 +0300

    open default page on start

--- .test-server.js
+++ .test-server.js
@@ -1,6 +1,7 @@
 module.exports = {
     bundles: 'bundles',
     levels: ['desktop.blocks'],
+    defaultTarget: '?.test.js',
     targets: {
         '?.js': ['js'],
         '?.test.js': ['test.js'],
